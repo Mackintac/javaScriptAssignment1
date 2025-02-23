edbf9d8180c77695a6332db57b2261ee7c031a6b
Mackintac <mack-robinson@hotmail.ca>
2025-02-23 17:22:36 -0500

    Fabricate arrays for use for different parts of story

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-function main() {}
+function main() {
+  // Array 1: Characters
+  const characters = [
+    'a curious scientist',
+    'a wandering knight',
+    'a mischievous elf',
+    'a brave adventurer',
+    'a retired pirate',
+    'a mysterious wizard',
+  ];
+
+  // Array 2: Setting
+  const settings = [
+    'in a hidden forest',
+    'on a distant mountain peak',
+    'deep inside a forgotten temple',
+    'underneath the city streets',
+    'in an abandoned castle',
+    'on the edge of a magical desert',
+  ];
+
+  // Array 3: Conflict
+  const conflicts = [
+    'seeking a long-lost artifact',
+    'fighting off an ancient curse',
+    'racing against time to save the kingdom',
+    'trying to escape a dangerous enemy',
+    'hunting down a rogue sorcerer',
+    'searching for a way to break an enchantment',
+  ];
+
+  // Array 4: Action
+  const actions = [
+    'uncovering an ancient secret',
+    'battling fierce monsters',
+    'deciphering cryptic runes',
+    'finding unexpected allies',
+    'defeating a powerful foe',
+    'making an impossible escape',
+  ];
+
+  // Array 5: Outcome
+  const outcomes = [
+    'discovering the true meaning of courage',
+    'unlocking a new realm of magic',
+    'finding a path back home',
+    'securing peace for the land',
+    'gaining a newfound power',
+    "earning a hero's place in legend",
+  ];
+}
